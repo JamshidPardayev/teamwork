65e7d812375664ea7df3d7c4438d5c2419bd71b7
Ilyosbek <abdurazzoqovilyosbek@gmail.com>
2025-08-23 11:57:39 +0500

    ccccc

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 
 const Layout = () => {
   return (
     <>
-      <Header />
       <main className="min-h-[40vh]">
         <Outlet />
       </main>
-      <Footer />
     </>
   );
 };
